bf6039368dfbcd7659019d4f279c26ed70940808
PedroLuisBrilhadori <pedroluisbrilhadori@gmail.com>
2023-06-21 20:32:33 -0300

    feat: initial commit

---
+++ src/main.ts
@@ -0,0 +1,11 @@
+import Fastify from "fastify";
+
+const fastify = Fastify({logger: true})
+
+fastify.get('/', async (req, res) => {
+  return {hello: 'world'}
+})
+
+
+fastify.listen({port: 3000});
+
