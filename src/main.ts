05b265609ab886caba2061c1046ceb97b2e514a6
PedroLuisBrilhadori <pedroluisbrilhadori@gmail.com>
2023-07-15 02:12:50 -0300

    feat: add vite ssr using react

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import chalk from "chalk";
 import dotenv from "dotenv";
 import { ExpressInstance, chessRoutes } from "./api";
+import { createServer as CreateViteServer } from 'vite';
+import path from 'path'
+import { readFileSync } from "fs";
 
 dotenv.config();
+
 
 export const start = async () => {
   const { PORT } = process.env;
@@ -11,6 +15,39 @@ export const start = async () => {
     const app = ExpressInstance();
 
     app.use("/api", await chessRoutes());
+
+    const vite = await CreateViteServer({
+      server: { middlewareMode: true },
+      appType: 'custom'
+    })
+
+    app.use(vite.middlewares)
+
+    app.use('*', async (req, res, next) => {
+      const url = req.originalUrl;
+
+      try {
+        let template = readFileSync(
+          path.resolve(__dirname + '/web/index.html'),
+          'utf-8'
+        )
+
+        template = await vite.transformIndexHtml(url, template)
+
+        const { render } = await vite.ssrLoadModule(
+          path.resolve(__dirname + "/web/entry-server.jsx")
+        )
+
+        const appHtml = await render(url);
+
+        const html = template.replace(`<!--ssr-outlet-->`, appHtml)
+
+        res.status(200).set({ 'Content-Type': 'text/html' }).end(html)
+      } catch (error) {
+        vite.ssrFixStacktrace(error as Error)
+        next(error)
+      }
+    })
 
     const port = PORT || 3000;
 
@@ -21,7 +58,7 @@ export const start = async () => {
       console.log(`${prefix} A API foi iniciada com sucesso!`);
       console.log(`${prefix} ${url}`);
     });
-  } catch (error) {}
+  } catch (error) { }
 };
 
 start();
---
+++ src/web/app.jsx
@@ -0,0 +1,11 @@
+import { Link, Route, Routes } from 'react-router-dom'
+
+export function App() {
+  return (
+    <>
+      <div>
+        ola tudo bem? que bom
+      </div>
+    </>
+  )
+}
